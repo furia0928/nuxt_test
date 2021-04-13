d28b87e5050e4091283cbc4d0948acd0a870bb5e
furia0928 <furia0928@gmail.com>
2021-04-13 18:14:55 +0900

    api test

--- pages/works/index.vue
+++ pages/works/index.vue
@@ -4,10 +4,9 @@
     <button @click="routerQueryUpdate(1)">123123</button>
     <button @click="$fetch">Refresh</button>-->
     {{ typeof page }}<br />
-    {{ query }}
-    <div v-for="item in test">
-      {{ item.id }}
-    </div>
+    {{ page }}<br />
+    {{ typeof query }}<br />
+    {{ query }}<br />
     <transition mode="out-in" name="list">
       <ul class="list" :key="$route.query.page" v-if="works">
         <li v-for="item in works" :key="item.id" class="list-item">
